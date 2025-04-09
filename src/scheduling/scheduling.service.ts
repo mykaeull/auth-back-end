f804da2a9425ebe0326988f94a4bca7a22cc2429
mykaeull <mykaeull99@gmail.com>
2025-04-09 20:21:40 -0300

    feat: adiciona tratamento de casos

--- src/scheduling/scheduling.service.ts
+++ src/scheduling/scheduling.service.ts
@@ -12,6 +12,17 @@ export class SchedulingService {
   ) {}
 
   async create(data: CreateSchedulingDto) {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const selectedDate = new Date(data.date);
+    selectedDate.setDate(selectedDate.getDate() + 1);
+    selectedDate.setHours(0, 0, 0, 0);
+
+    if (selectedDate < today) {
+      throw new Error('Não é possível agendar para datas anteriores à atual.');
+    }
+
     const amount = data.type === 'corte' ? 30 : data.type === 'barba' ? 15 : 40;
 
     const paid = data.payment_method === 'pix';
@@ -30,6 +41,18 @@ export class SchedulingService {
       return []; // validação básica
     }
 
+    const today = new Date();
+    const todayStr = today.toISOString().split('T')[0];
+
+    if (date < todayStr) {
+      return []; // bloqueia datas anteriores
+    }
+
+    const isToday = date === todayStr;
+
+    const currentHour = today.getHours();
+    const currentMinutes = today.getMinutes();
+
     const agendamentos = await this.schedulingRepository.find({
       where: { date },
     });
@@ -37,17 +60,33 @@ export class SchedulingService {
     const horariosIndisponiveis = agendamentos.map((ag) => ag.time);
 
     const horariosDisponiveis: string[] = [];
-
     const [openHour, closeHour] = [9, 19]; // 09:00 até 19:00
 
-    for (let hour = openHour; hour < closeHour; hour++) {
-      horariosDisponiveis.push(`${String(hour).padStart(2, '0')}:00`);
-      horariosDisponiveis.push(`${String(hour).padStart(2, '0')}:30`);
-    }
-    horariosDisponiveis.push('19:00'); // inclui 19:00
+    for (let hour = openHour; hour <= closeHour; hour++) {
+      const horaCheia = `${String(hour).padStart(2, '0')}:00`;
+      const meiaHora = `${String(hour).padStart(2, '0')}:30`;
 
-    return horariosDisponiveis.filter(
-      (hora) => !horariosIndisponiveis.includes(hora),
-    );
+      if (
+        !isToday ||
+        hour > currentHour ||
+        (hour === currentHour && currentMinutes < 30)
+      ) {
+        if (!horariosIndisponiveis.includes(horaCheia)) {
+          horariosDisponiveis.push(horaCheia);
+        }
+      }
+
+      if (
+        !isToday ||
+        hour > currentHour ||
+        (hour === currentHour && currentMinutes < 0)
+      ) {
+        if (!horariosIndisponiveis.includes(meiaHora)) {
+          horariosDisponiveis.push(meiaHora);
+        }
+      }
+    }
+
+    return horariosDisponiveis;
   }
 }
